4a058b6153fb6fba17574730a3482b1df943fa6c
archanababuraj99 <mca509@rajagiri.edu>
2021-03-17 11:52:55 +0530

    Student laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    
+    <div class="container">
+    <div class="row">
+    <div class="col col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2"></div>
+    <div class="col col-12 col-sm-7 col-md-7 col-lg-7 col-xl-7 col-xxl-7">
+    <table class="table">
+    <tr>
+    <h1>Register Here!!</h1>
+        <td>Name</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Roll No</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Admission No</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>College</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><button class="btn btn-warning">Register</button></td>
+    </tr>
+    </table>
+    </div>
+    <div class="col col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3"></div>
+    </div>
+    </div>
+</body>
+</html>
